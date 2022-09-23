0a5502da2b4dcadb3451fdb5de7d8cbbb2ba87bb
mahmoud qunbor <modesq.99q@gmail.com>
2022-09-23 21:32:37 +0300

    for deployment

--- src/components/mapData.js
+++ src/components/mapData.js
@@ -18,16 +18,6 @@ class MapData extends React.Component {
                     </Card.Body>
                 </Card>
             </div>
-
-
-            // <div>
-            //     <h3>city name : {this.props.cityName}</h3>
-            //     <p>Lon : {this.props.lon}</p>
-            //     <p>Lat : {this.props.lat}</p>
-
-            //     {this.props.mapFlag && <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.bf868f2123453ea99fec1e16c3a67189&center=${this.props.lat},${this.props.lon}`} alt="map" ></img>}
-            //     {this.props.errFlag && <h4>Error : sorry something went wrong!</h4>}
-            // </div>
         );
     }
 }
